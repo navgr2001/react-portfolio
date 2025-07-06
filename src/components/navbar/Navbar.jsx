31dc06f98c1b3d06b31d9989aacb9128a662ce34
naveengr2001 <138234805+navgr2001@users.noreply.github.com>
2025-07-06 21:04:26 +0530

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,17 +16,17 @@ const Navbar = () => {
           Naveen Grero
         </motion.span>
         <div className="social">
-          <a href="#">
-            <img src="/facebook.png" alt="" />
+          <a href="https://www.facebook.com/shalomnaveen" target="_blank" rel="noopener noreferrer">
+            <img src={`${import.meta.env.BASE_URL}facebook.png`} alt="Facebook" />
           </a>
-          <a href="#">
-            <img src="/instagram.png" alt="" />
+          <a href="https://www.instagram.com/shalom_naveen?igsh=MXgydHVzeWJsN2hyaw==" target="_blank" rel="noopener noreferrer">
+            <img src={`${import.meta.env.BASE_URL}instagram.png`} alt="Instagram" />
           </a>
-          <a href="#">
-            <img src="/youtube.png" alt="" />
+          <a href="https://www.linkedin.com/in/naveen-grero-6a6120207" target="_blank" rel="noopener noreferrer">
+            <img src={`${import.meta.env.BASE_URL}linkedin.png`} alt="Linkedin" />
           </a>
-          <a href="#">
-            <img src="/dribbble.png" alt="" />
+          <a href="https://github.com/navgr2001" target="_blank" rel="noopener noreferrer">
+            <img src={`${import.meta.env.BASE_URL}github.png`} alt="Github" />
           </a>
         </div>
       </div>
