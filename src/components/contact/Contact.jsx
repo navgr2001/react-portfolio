24027a51533665529910f405cd9bceaa7da8e876
naveengr2001 <138234805+navgr2001@users.noreply.github.com>
2025-07-06 17:43:55 +0530

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -58,15 +58,15 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>naveenshalom2001@gmail.com</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Hello street New York</span>
+          <span>No 42, Wattawa road, Amandoluwa, Seeduwa</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
-          <span>+1 234 5678</span>
+          <span>(+94) 76 452 5012</span>
         </motion.div>
       </motion.div>
       <div className="formContainer">
