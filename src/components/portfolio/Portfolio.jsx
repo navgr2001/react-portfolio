702dd5415de3268032adb80c46e90ded2b08e656
naveengr2001 <138234805+navgr2001@users.noreply.github.com>
2025-07-07 16:20:26 +0530

    spell

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,7 +6,7 @@ const items = [
   {
     id: 1,
     title: "React Portfolio",
-    img:`${import.meta.env.BASE_URL}reactprtfolio.PNG`,
+    img:`${import.meta.env.BASE_URL}reactportfolio.PNG`,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
   {
